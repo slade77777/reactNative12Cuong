121777a3385bfe48a37578705fd2b7543cb8360b
Rindaaaa <nmd0909hp@gmail.com>
2021-06-28 21:02:37 +0700

    Duc sectionList

--- App.js
+++ App.js
@@ -1,133 +1,77 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow strict-local
- */
+import React from "react";
+import { StyleSheet, Text, View, SafeAreaView, SectionList, StatusBar, TextInput } from "react-native";
 
-import React from 'react';
-import type {Node} from 'react';
-import {
-  SafeAreaView,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-  FlatList,
-  Image, Dimensions,
-} from "react-native";
+const DATA = [
+  {
+    title: "A",
+    data: ["Anh Binh", "Anh chu nha", "Anh Gia"]
+  },
+  {
+    title: "B",
+    data: ["Ba Ngoai", "Bac Dinh", "Bac Hoan"]
+  },
+  {
+    title: "C",
+    data: ["Cau Phuong", "Co Kieu Anh", "Co Thi"]
+  },
+  {
+    title: "D",
+    data: ["Dau", "Duy Hieu", "Duong Ch"]
+  }
 
-import {Colors} from 'react-native/Libraries/NewAppScreen';
-
-const data = [
-  {
-    name: 'tai nghe',
-    content: '172k san pham',
-    imageUrl:
-      'https://cdn.pixabay.com/photo/2015/12/01/20/28/road-1072823__340.jpg',
-  },
-  {
-    name: 'quan ao',
-    content: '50k san pham',
-    imageUrl:
-      'https://cdn.pixabay.com/photo/2015/12/01/20/28/road-1072823__340.jpg',
-  },
-  {
-    name: 'may tinh',
-    content: '20k san pham',
-    imageUrl:
-      'https://cdn.pixabay.com/photo/2015/12/01/20/28/road-1072823__340.jpg',
-  },
-  {
-    name: 'giay dep',
-    content: '500k san pham',
-    imageUrl:
-      'https://cdn.pixabay.com/photo/2015/12/01/20/28/road-1072823__340.jpg',
-  },
 ];
 
-const App: () => Node = () => {
-  const isDarkMode = useColorScheme() === 'dark';
+const Item = ({ title }) => (
+  <View style={styles.item}>
+    <Text style={styles.title}>{title}</Text>
+  </View>
+);
 
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
-  return (
-    <SafeAreaView
-      style={{
-        backgroundColor: '#F8F8F8',
-        flex: 1,
-        justifyContent: 'center',
-      }}>
-      <View style={{backgroundColor: 'white'}}>
-        <View
-          style={{
-            flexDirection: 'row',
-            justifyContent: 'space-between',
-            padding: 10,
-          }}>
-          <Text style={{fontSize: 20, color: 'orange', fontWeight: 'bold'}}>
-            Tìm kiếm phổ biến
-          </Text>
-          <Text style={{color: 'orange'}}>Xem thêm</Text>
-        </View>
-        <View
-          style={{
-            flexDirection: 'row',
-            flexWrap: 'wrap',
-          }}>
-          {data.map((item, index) => {
-            return (
-              <View key={index} style={styles.product}>
-                <View>
-                  <Text>{item.name}</Text>
-                  <Text>{item.content}</Text>
-                </View>
-                <Image
-                  source={{
-                    uri: item.imageUrl,
-                  }}
-                  style={{width: 50, height: 50}}
-                />
-              </View>
-            );
-          })}
-        </View>
-        <FlatList
-          data={data}
-          numColumns={2}
-          keyExtractor={item => item.name}
-          renderItem={({item}, index) => (
-            <View key={index} style={styles.product}>
-              <View>
-                <Text>{item.name}</Text>
-                <Text>{item.content}</Text>
-              </View>
-              <Image
-                source={{
-                  uri: item.imageUrl,
-                }}
-                style={{width: 50, height: 50}}
-              />
-            </View>
-          )}
-        />
-      </View>
-    </SafeAreaView>
-  );
-};
+const App = () => (
+  <SafeAreaView style={styles.container}>
+    <TextInput
+      style={styles.border}
+      placeholder="Tim kiem"
+    />
+    <SectionList
+      sections={DATA}
+      keyExtractor={(item, index) => item + index}
+      renderItem={({ item }) => <Item title={item} />}
+      renderSectionHeader={({ section: { title } }) => (
+        <Text style={styles.header}>{title}</Text>
+      )}
+    />
+  </SafeAreaView>
+);
 
 const styles = StyleSheet.create({
-  product: {
-    borderWidth: 1,
-    borderColor: 'grey',
-    padding: 10,
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    backgroundColor: 'yellow',
-    width: Dimensions.get('window').width * 0.5,
+  container: {
+    flex: 1,
+    paddingTop: StatusBar.currentHeight,
+    backgroundColor: '#11AC93'
   },
+  item: {
+    padding: 15,
+    marginVertical: 8
+  },
+  header: {
+    fontSize: 30,
+    backgroundColor: "#38C3B7",
+    color: 'white',
+    marginHorizontal: 10,
+  },
+  title: {
+    fontSize: 18,
+    color: 'white',
+  },
+  border: {
+    backgroundColor: 'white',
+    marginHorizontal: 10,
+    marginVertical: 8,
+    borderWidth: 1,
+    borderRadius: 12,
+    borderColor: '#11AC93'
+  }
 });
 
 export default App;
