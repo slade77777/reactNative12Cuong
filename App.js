4cd0bd595b8a1910fde204655d9ac60d21d642f5
100B <toannguyenas@gmail.com>
2021-06-28 21:01:46 +0700

    Toan Section List

--- App.js
+++ App.js
@@ -1,133 +1,137 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow strict-local
- */
+import React from "react";
+import { StyleSheet, Text, View, SafeAreaView, SectionList, StatusBar } from "react-native";
 
-import React from 'react';
-import type {Node} from 'react';
-import {
-  SafeAreaView,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-  FlatList,
-  Image, Dimensions,
-} from "react-native";
 
-import {Colors} from 'react-native/Libraries/NewAppScreen';
+// const data = [
+//   {
+//     name: 'tai nghe',
+//     content: '172k san pham',
+//     imageUrl:
+//       'https://cdn.pixabay.com/photo/2015/12/01/20/28/road-1072823__340.jpg',
+//   },
+//   {
+//     name: 'quan ao',
+//     content: '50k san pham',
+//     imageUrl:
+//       'https://cdn.pixabay.com/photo/2015/12/01/20/28/road-1072823__340.jpg',
+//   },
+//   {
+//     name: 'may tinh',
+//     content: '20k san pham',
+//     imageUrl:
+//       'https://cdn.pixabay.com/photo/2015/12/01/20/28/road-1072823__340.jpg',
+//   },
+//   {
+//     name: 'giay dep',
+//     content: '500k san pham',
+//     imageUrl:
+//       'https://cdn.pixabay.com/photo/2015/12/01/20/28/road-1072823__340.jpg',
+//   },
+// ];
 
-const data = [
+
+
+
+
+
+
+
+// const DATA = [
+//   {
+//     title:"A",
+//     data: [
+//       {name:"Anh rina Haro", phoneNumber:"+84 2125 1256"}
+//     ]
+//   },
+//   {
+//     title:"B",
+//     data: [
+//       {name:"Ban shina", phoneNumber:"(+84)97 5892 666"},
+//       {name:"Binh Nguyen", phoneNumber:"(+84)98 966 8759"},
+//       {name:"Ban Do", phoneNumber:"(+84) 95458 653214"},
+//     ]
+//   },
+//   {
+//     title:"C",
+//     data: [
+//       {name:"Cong Pham", phoneNumber:"(+84) 987 568 6895"},
+//       {name:"Chinh Ngo", phoneNumber:"(+84) 975 8889 523"},
+//       {name:"Cam Nguyen", phoneNumber:"(+84) 895 6332 513"},
+//     ]
+//   },
+// ];
+
+// const Item = ({title}) => (
+//   <View>
+//     <Text>{title}</Text>
+//   </View>
+// )
+
+// const App = () => (
+//   <SafeAreaView>
+//     <SectionList
+//       sections={DATA}
+//       keyExtractor={(item, index) => item + index}
+//       renderItem={({ item }) => <Item title={item} />}
+//       renderSectionHeader={({ section: { title } }) => (
+//         <Text>{title}</Text>
+//       )}
+//     />
+//   </SafeAreaView>
+// );
+
+
+const DATA = [
   {
-    name: 'tai nghe',
-    content: '172k san pham',
-    imageUrl:
-      'https://cdn.pixabay.com/photo/2015/12/01/20/28/road-1072823__340.jpg',
+    title: "A",
+    data: [
+      {name:"Anh rina Haro", phoneNumber:"+84 2125 1256"}
+    ]
   },
   {
-    name: 'quan ao',
-    content: '50k san pham',
-    imageUrl:
-      'https://cdn.pixabay.com/photo/2015/12/01/20/28/road-1072823__340.jpg',
+    title: "B",
+    data: [
+      {name:"Anh rina Haro", phoneNumber:"+84 2125 1256"}
+    ]
   },
   {
-    name: 'may tinh',
-    content: '20k san pham',
-    imageUrl:
-      'https://cdn.pixabay.com/photo/2015/12/01/20/28/road-1072823__340.jpg',
+    title: "C",
+    data: [
+      {name:"Anh rina Haro", phoneNumber:"+84 2125 1256"}
+    ]
   },
   {
-    name: 'giay dep',
-    content: '500k san pham',
-    imageUrl:
-      'https://cdn.pixabay.com/photo/2015/12/01/20/28/road-1072823__340.jpg',
-  },
+    title: "D",
+    data: [
+      {name:"Anh rina Haro", phoneNumber:"+84 2125 1256"}
+    ]
+  }
 ];
 
-const App: () => Node = () => {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
+const Item = ({ item }) => {
+  console.log(item)
   return (
-    <SafeAreaView
-      style={{
-        backgroundColor: '#F8F8F8',
-        flex: 1,
-        justifyContent: 'center',
-      }}>
-      <View style={{backgroundColor: 'white'}}>
-        <View
-          style={{
-            flexDirection: 'row',
-            justifyContent: 'space-between',
-            padding: 10,
-          }}>
-          <Text style={{fontSize: 20, color: 'orange', fontWeight: 'bold'}}>
-            Tìm kiếm phổ biến
-          </Text>
-          <Text style={{color: 'orange'}}>Xem thêm</Text>
-        </View>
-        <View
-          style={{
-            flexDirection: 'row',
-            flexWrap: 'wrap',
-          }}>
-          {data.map((item, index) => {
-            return (
-              <View key={index} style={styles.product}>
-                <View>
-                  <Text>{item.name}</Text>
-                  <Text>{item.content}</Text>
-                </View>
-                <Image
-                  source={{
-                    uri: item.imageUrl,
-                  }}
-                  style={{width: 50, height: 50}}
-                />
-              </View>
-            );
-          })}
-        </View>
-        <FlatList
-          data={data}
-          numColumns={2}
-          keyExtractor={item => item.name}
-          renderItem={({item}, index) => (
-            <View key={index} style={styles.product}>
-              <View>
-                <Text>{item.name}</Text>
-                <Text>{item.content}</Text>
-              </View>
-              <Image
-                source={{
-                  uri: item.imageUrl,
-                }}
-                style={{width: 50, height: 50}}
-              />
-            </View>
-          )}
-        />
-      </View>
-    </SafeAreaView>
-  );
-};
+  <View>
+    <Text>{item.name}</Text>
+  </View>
+)};
+
+const App = () => (
+  <SafeAreaView>
+    <SectionList
+      sections={DATA}
+      keyExtractor={(item, index) => item + index}
+      renderItem={({ item }) => <Item item={item} />}
+      renderSectionHeader={({ section: { title } }) => (
+        <Text>{title}</Text>
+      )}
+    />
+  </SafeAreaView>
+);
+
 
 const styles = StyleSheet.create({
-  product: {
-    borderWidth: 1,
-    borderColor: 'grey',
-    padding: 10,
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    backgroundColor: 'yellow',
-    width: Dimensions.get('window').width * 0.5,
-  },
+
 });
 
 export default App;
